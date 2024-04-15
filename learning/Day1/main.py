43260adf22e597cc4ff85077f347eec7298f9c7e
VINANK <neupaneayush3@gmail.com>
2024-04-15 16:34:28 +0545

    introduction to fastapi

--- learning/Day1/main.py
+++ learning/Day1/main.py
@@ -1,71 +1,17 @@
 from fastapi import FastAPI
-from enum import Enum
-from pydantic import BaseModel
-from typing import Union
 
 app=FastAPI()
 
-
-# to print hello world in fast api
-@app.get("/")
-async def root():
-	return {"message":"hello world"}
-
-# get request with path parameter
-@app.get("/items/{item_id}")
-async def item(item_id):
-	return {"message":item_id}
+@app.get("/",description="this is the get route")
+def get():
+    return {"message":"hello form the get route"}
 
 
-# creating a enum
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
+@app.post("/")
+def post():
+    return {"message":"hello form the post route"}
 
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
-
-@app.get("/item/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
-
-
-from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-
-
-app = FastAPI()
-
-
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
-
-from typing import Annotated
-
-from fastapi import FastAPI, Form
-
-# app = FastAPI()
-
-
-@app.post("/login/")
-async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
-    return {"username": username}
+@app.put("/")
+def put():
+    return {"message":"hello form the put route"}
